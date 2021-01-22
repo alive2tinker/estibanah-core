30ac0538868aaac2e479455d9a470a5612f963d9
alive2tinker <alsufiran@gmail.com>
2021-01-22 23:46:19 +0300

    fix #2

--- resources/js/components/FormResponseComponent.vue
+++ resources/js/components/FormResponseComponent.vue
@@ -1,37 +1,48 @@
 <template>
-    <div class="container">
-        <div class="alert alert-danger alert-dismissible fade show" role="alert" v-for="error in errors">
-            <strong>Oops!</strong> {{  error  }}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <h1>{{ title }}</h1>
-        <p class="text-muted">{{ description }}</p>
-        <div class="card" v-for="response in responses" :key="response.question.id" v-show="parse(response.question.conditions)">
-            <div class="card-body">
-                <h3 class="card-title">{{ response.question.text }}</h3>
-                <input type="text" class="form-control" v-show="response.question.type === 'short_answer'" v-model="response.value">
-                <input type="date" class="form-control" v-show="response.question.type === 'date'" v-model="response.value">
-                <input type="file" class="form-control" v-show="response.question.type === 'file'" ref="file" multiple @change="pushFiles($event, response)">
-                <textarea v-model="response.value" cols="30" rows="10" class="form-control" v-show="response.question.type === 'long_answer'"></textarea>
-                <div class="form-check"
-                     v-show="response.question.type === 'multiple_choice'"
-                     v-for="answer in response.question.answers" :key="answer.id">
-                    <input type="radio" :name="`response-${response.id}-answers`" :id="`answer-${answer.id}`" :value="answer.text" v-model="response.value">
-                    <label :for="`one-${answer.id}`">{{  answer.text  }}</label>
-                </div>
-                <div class="form-check"
-                     v-show="response.question.type === 'checkbox'"
-                     v-for="answer in response.question.answers" :key="`answer-${answer.id}`">
-                    <input type="checkbox" :value="answer.text" v-model="response.value">
-                    <label :for="`one-${answer.id}`">{{  answer.text  }}</label>
-                </div>
+    <div>
+        <div class="loader-background" v-if="isLoading">
+            <div class="loader-container">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
         </div>
-        <div class="row py-3 justify-content-center">
-            <div class="col-md-5">
-                <button type="button" class="btn btn-primary btn-block btn-lg" @click="submitResponse($event)">Submit</button>
+        <div class="container">
+            <h1>{{ title }}</h1>
+            <p class="text-muted">{{ description !== "null" ? description : 'no description' }}</p>
+            <div :class="{'card': true, 'hasError': hasError(index)}" v-for="(response, index) in responses" :key="response.question.id" v-show="parse(response.question.conditions)">
+                <div class="card-body">
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="hasError(index)">
+                        <strong>Oops!</strong> {{  getErrorMessage(index)  }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <h3 class="card-title">{{ response.question.text }}</h3>
+                    <input type="text" class="form-control" v-show="response.question.type === 'short_answer'" v-model="response.value">
+                    <input type="date" class="form-control" v-show="response.question.type === 'date'" v-model="response.value">
+                    <input type="file" class="form-control" v-show="response.question.type === 'file'" ref="file" multiple @change="pushFiles($event, response)">
+                    <textarea v-model="response.value" cols="30" rows="10" class="form-control" v-show="response.question.type === 'long_answer'"></textarea>
+                    <div class="form-check"
+                         v-show="response.question.type === 'multiple_choice'"
+                         v-for="answer in response.question.answers" :key="answer.id">
+                        <input type="radio" :name="`response-${response.id}-answers`" :id="`answer-${answer.id}`" :value="answer.text" v-model="response.value">
+                        <label :for="`one-${answer.id}`">{{  answer.text  }}</label>
+                    </div>
+                    <div class="form-check"
+                         v-show="response.question.type === 'checkbox'"
+                         v-for="answer in response.question.answers" :key="`answer-${answer.id}`">
+                        <input type="checkbox" :value="answer.text" v-model="response.value">
+                        <label :for="`one-${answer.id}`">{{  answer.text  }}</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-3 justify-content-center">
+                <div class="col-md-5">
+                    <button type="button" class="btn btn-primary btn-block btn-lg" @click="submitResponse($event)">Submit</button>
+                </div>
             </div>
         </div>
     </div>
@@ -44,7 +55,7 @@ export default {
         return {
             title: '',
             description: '',
-            loading: true,
+            isLoading: true,
             responses: [],
             errors: [],
             files: []
@@ -55,6 +66,7 @@ export default {
     },
     methods: {
         syncData: function(){
+            this.isLoading = true;
             axios.get(`/forms/resource/${this.form}`).then((response) => {
                 this.title = response.data.data.title;
                 this.description = response.data.data.description;
@@ -64,7 +76,7 @@ export default {
                     else
                         this.responses.push({question: q, value: ''})
                 })
-                this.loading = false;
+                this.isLoading = false;
             });
         },
         parse: function(conditions){
@@ -96,10 +108,10 @@ export default {
         isAValidResponse: function(){
             var res = true;
 
-            this.responses.forEach(response => {
+            this.responses.forEach((response, index) => {
                 if(response.question.required && (response.value === '' || response.value === [])){
                     window.scrollTo(0,0);
-                    this.errors.push(`question: ${response.question.text} is required!`);
+                    this.errors.push({key:index, message: `${response.question.text} is required`});
                     res = res && false;
                 }
             });
@@ -115,10 +127,33 @@ export default {
                 });
                 formData.append('responses', JSON.stringify(this.responses));
                 axios.post(`/forms/${this.form}`, formData).then((response) => {
-                    console.log(response);
+                    this.$swal.fire({
+                        title: "Great!",
+                        text: "Form created successfully! redirecting you shortly",
+                        icon: "success"
+                    }).then((result) => {
+                        if(result.isConfirmed)
+                            window.location = '/forms/thanks';
+                    });
                 })
             }
-        }
+        },
+        hasError: function(index){
+            var res = false;
+            this.errors.forEach((err) => {
+                if(err.key === index)
+                    res= true;
+            });
+            return res;
+        },
+        getErrorMessage: function(index){
+            var res = false;
+            this.errors.forEach((err) => {
+                if(err.key === index)
+                    res = err.message;
+            });
+            return res;
+        },
     }
 }
 </script>
@@ -129,5 +164,47 @@ export default {
     @keyframes fadeIn {
         from{opacity: 0;}
         to{opacity: 1}
+    }
+    .loader-container {
+        width: 144px;
+        height: 100px;
+        position: relative;
+        margin: 0 auto;
+    }
+    .loader-container span {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        display: block;
+        background-color: #9B5986;
+        animation: Loading 3s infinite ease-in-out;
+    }
+    .loader-container span:nth-of-type(2) { left: 30px; animation-delay: .2s; }
+    .loader-container span:nth-of-type(3) { left: 60px; animation-delay: .4s; }
+    .loader-container span:nth-of-type(4) { left: 90px; animation-delay: .6s; }
+    .loader-container span:last-of-type { left: 120px; animation-delay: .8s; }
+
+    @keyframes Loading {
+        0% { height: 20px; transform: translateY(0); background-color: #9B59B6; }
+        50% { height: 20px; transform: translateY(40px); background-color: #3498D6; }
+        100% { height: 20px; transform: translateY(0); background-color: #9B59B6; }
+    }
+    .loader-background{
+        background: rgba(255,255,255,0.6);
+        z-index: 99999999999;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        position: absolute;
+        padding-top: 25%;
+    }
+    .hasError{
+        border: 1px solid red;
+    }
+    .hasError > div.body > input{
+        border: 1px solid red;
     }
 </style>
